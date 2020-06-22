2e3c5529fd8ca5e64ee7771e7c1ef14d9db7268e
Fi Đại <43561662+ff3105@users.noreply.github.com>
2020-06-22 20:51:57 +0700

    Add files via upload

---
+++ hanghoa.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 22, 2020 lúc 01:51 PM
+-- Phiên bản máy phục vụ: 10.4.8-MariaDB
+-- Phiên bản PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `android_hanghoa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hanghoa`
+--
+
+CREATE TABLE `hanghoa` (
+  `id` int(11) NOT NULL,
+  `ten_sp` varchar(1024) NOT NULL,
+  `gia_sp` int(255) NOT NULL,
+  `anh_sp` varchar(1024) NOT NULL,
+  `mota_sp` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `hanghoa`
+--
+
+INSERT INTO `hanghoa` (`id`, `ten_sp`, `gia_sp`, `anh_sp`, `mota_sp`) VALUES
+(12, 'Đại', 3, '2', '4'),
+(13, '2', 4, '3', '534'),
+(14, '', 0, 'vlcsnap-2020-01-07-01h05m48s883.png', ''),
+(15, '', 0, 'vlcsnap-2020-01-07-01h05m48s883.png', '');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `hanghoa`
+--
+ALTER TABLE `hanghoa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `hanghoa`
+--
+ALTER TABLE `hanghoa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
